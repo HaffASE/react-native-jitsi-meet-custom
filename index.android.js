488cef1c8300141f3ec66de3eb3715113c00487c
Sergei Kustikov <haffase@gmail.com>
2021-04-23 16:50:38 +0300

    refactor(android): remove end call bridge

--- index.android.js
+++ index.android.js
@@ -6,14 +6,11 @@ import { NativeModules, requireNativeComponent } from 'react-native';
 
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetModule;
+
 const call = JitsiMeetModule.call;
-const endCall = JitsiMeetModule.endCall;
 JitsiMeetModule.call = (url, userInfo) => {
   userInfo = userInfo || {};
   call(url, userInfo);
-};
-JitsiMeetModule.endCall = () => {
-  endCall();
 };
 
 export default JitsiMeetModule;
