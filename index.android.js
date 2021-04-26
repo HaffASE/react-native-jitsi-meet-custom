c74b93b240be990f94aceaaa56a0d7af42ab3e6e
Sergei Kustikov <haffase@gmail.com>
2021-04-26 16:06:13 +0300

    fix(android): add props for bridge call

--- index.android.js
+++ index.android.js
@@ -8,9 +8,9 @@ export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetModule;
 
 const call = JitsiMeetModule.call;
-JitsiMeetModule.call = (url, userInfo) => {
+JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
   userInfo = userInfo || {};
-  call(url, userInfo);
+  call(url, userInfo, meetOptions, meetFeatureFlags);
 };
 
 export default JitsiMeetModule;
