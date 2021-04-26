fd3f2bd1f5858f992ed539715b7b1ae3e40cf9c6
Sergei Kustikov <haffase@gmail.com>
2021-04-26 16:00:42 +0300

    feat(ios): add meet features flags for call

--- index.ios.js
+++ index.ios.js
@@ -6,9 +6,10 @@ import { NativeModules, requireNativeComponent } from 'react-native';
 
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetView;
+
 const call = JitsiMeetModule.call;
-JitsiMeetModule.call = (url, userInfo) => {
+JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
   userInfo = userInfo || {};
-  call(url, userInfo);
+  call(url, userInfo, meetOptions, meetFeatureFlags);
 };
 export default JitsiMeetModule;
--- ios/RNJitsiMeetViewManager.m
+++ ios/RNJitsiMeetViewManager.m
@@ -24,7 +24,12 @@ RCT_EXPORT_METHOD(initialize)
     RCTLogInfo(@"Initialize is deprecated in v2");
 }
 
-RCT_EXPORT_METHOD(call:(NSString *)urlString userInfo:(NSDictionary *)userInfo)
+RCT_EXPORT_METHOD(
+    call:(NSString *)urlString
+    userInfo:(NSDictionary *)userInfo
+    meetOptions:(NSDictionary *)meetOptions
+    meetFeatureFlags:(NSDictionary *)meetFeatureFlags
+)
 {
     RCTLogInfo(@"Load URL %@", urlString);
     JitsiMeetUserInfo * _userInfo = [[JitsiMeetUserInfo alloc] init];
@@ -40,30 +45,30 @@ RCT_EXPORT_METHOD(call:(NSString *)urlString userInfo:(NSDictionary *)userInfo)
     dispatch_sync(dispatch_get_main_queue(), ^{
         JitsiMeetConferenceOptions *options = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {
             builder.room = urlString;
-            builder.userInfo = _userInfo;
-        }];
-        [jitsiMeetView join:options];
-    });
-}
+            builder.token = meetOptions[@"token"];
+            builder.subject = meetOptions[@"subject"];
+            builder.videoMuted = [[meetOptions objectForKey:@"videoMuted"] boolValue];
+            builder.audioOnly = [[meetOptions objectForKey:@"audioOnly"] boolValue];
+            builder.audioMuted = [[meetOptions objectForKey:@"audioMuted"] boolValue];
 
-RCT_EXPORT_METHOD(audioCall:(NSString *)urlString userInfo:(NSDictionary *)userInfo)
-{
-    RCTLogInfo(@"Load Audio only URL %@", urlString);
-    JitsiMeetUserInfo * _userInfo = [[JitsiMeetUserInfo alloc] init];
-    if (userInfo != NULL) {
-      if (userInfo[@"displayName"] != NULL) {
-        _userInfo.displayName = userInfo[@"displayName"];
-      }
-      if (userInfo[@"avatar"] != NULL) {
-        NSURL *url = [NSURL URLWithString:[userInfo[@"avatar"] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
-        _userInfo.avatar = url;
-      }
-    }
-    dispatch_sync(dispatch_get_main_queue(), ^{
-        JitsiMeetConferenceOptions *options = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {        
-            builder.room = urlString;
+            [builder setFeatureFlag:@"add-people.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"add-people.enabled"] boolValue]];
+            [builder setFeatureFlag:@"calendar.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"calendar.enabled"] boolValue]];
+            [builder setFeatureFlag:@"call-integration.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"call-integration.enabled"] boolValue]];
+            [builder setFeatureFlag:@"chat.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"chat.enabled"] boolValue]];
+            [builder setFeatureFlag:@"close-captions.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"close-captions.enabled"] boolValue]];
+            [builder setFeatureFlag:@"invite.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"invite.enabled"] boolValue]];
+            [builder setFeatureFlag:@"ios.recording.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"ios.recording.enabled"] boolValue]];
+            [builder setFeatureFlag:@"live-streaming.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"live-streaming.enabled"] boolValue]];
+            [builder setFeatureFlag:@"meeting-name.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"meeting-name.enabled"] boolValue]];
+            [builder setFeatureFlag:@"meeting-password.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"meeting-password.enabled"] boolValue]];
+            [builder setFeatureFlag:@"pip.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"pip.enabled"] boolValue]];
+            [builder setFeatureFlag:@"raise-hand.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"raise-hand.enabled"] boolValue]];
+            [builder setFeatureFlag:@"recording.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"recording.enabled"] boolValue]];
+            [builder setFeatureFlag:@"tile-view.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"tile-view.enabled"] boolValue]];
+            [builder setFeatureFlag:@"toolbox.alwaysVisible" withBoolean:[[meetFeatureFlags objectForKey:@"toolbox.alwaysVisible"] boolValue]];
+            [builder setFeatureFlag:@"welcomepage.enabled" withBoolean:[[meetFeatureFlags objectForKey:@"welcomepage.enabled"] boolValue]];
+
             builder.userInfo = _userInfo;
-            builder.audioOnly = YES;
         }];
         [jitsiMeetView join:options];
     });
